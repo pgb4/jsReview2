ceb77e580272a5e001dc351f38ac55062e9eacf5
pgb4 <preston.bebas1@gmail.com>
2018-12-17 20:59:03 -0500

    ridiculously simplified code

--- script.js
+++ script.js
@@ -1,40 +1,17 @@
-var h3 = document.querySelector("h3")
+var h3 = document.querySelector("h3");
 
-var color1btn = document.querySelectorAll("input")[0]
+var color1 = document.querySelector(".color1");
 
-var color2btn = document.querySelectorAll("input")[1]
-
-var color1 = document.querySelectorAll("input")[0].value
-
-var color2 = document.querySelectorAll("input")[1].value
+var color2 = document.querySelector(".color2");
 
 var body = document.querySelector("body")
 
-function displayColorHexCodes(color1, color2) {
-    h3.textContent=`${color1}, ${color2}`;
+
+function changeColor() {
+    body.style.background=`linear-gradient(to right, ${color1.value} , ${color2.value})`
+    h3.textContent=`${color1.value}, ${color2.value}`;
 }
 
-function updateColorValue(event) {
-    if(event.target===color1btn){
-        color1 = event.target.value
-    } else if (event.target===color2btn) {
-        color2 = event.target.value
-    }
-} 
+color1.addEventListener("change", changeColor)
 
-function changeBackgroundColorDisplay(color1, color2) {
-    body.style.background=`linear-gradient(to right, ${color1} , ${color2})`
-}
-
-function changeColor(event) {
-    updateColorValue(event)
-    changeBackgroundColorDisplay(color1, color2)
-    displayColorHexCodes(color1, color2)
-}
-
-color1btn.addEventListener("change", changeColor)
-
-color2btn.addEventListener("change", changeColor)
-
-displayColorHexCodes(color1, color2);
-
+color2.addEventListener("change", changeColor)
